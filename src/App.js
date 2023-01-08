0412a53bbc66d0609cd03ee7795ba2eff1a39218
kostadinov1 <evgenikostadinov1987@gmail.com>
2023-01-08 18:10:14 +0000

    added basic home page and main.css

--- src/App.js
+++ src/App.js
@@ -1,83 +1,146 @@
-import React, { useState } from 'react';
-import {
-  DesktopOutlined,
-  FileOutlined,
-  PieChartOutlined,
-  TeamOutlined,
-  UserOutlined,
-} from '@ant-design/icons';
+
+
+import { Link, Route, Routes } from 'react-router-dom';
+import React from 'react';
 import { Breadcrumb, Layout, Menu, theme } from 'antd';
-import Link from 'antd/es/typography/Link';
-import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
-const { Header, Content, Footer, Sider } = Layout;
-function getItem(label, key, icon, children) {
-  return {
-    key,
-    icon,
-    children,
-    label,
-  };
-}
-const items = [
-  getItem(<Link to='/'>Home</Link>, '1', <PieChartOutlined />),
-  getItem(<Link to='/'>Projects</Link>, '2', <DesktopOutlined />),
-  // getItem('User', 'sub1', <UserOutlined />, [
-  //   getItem('Tom', '3'),
-  //   getItem('Bill', '4'),
-  //   getItem('Alex', '5'),
-  // ]),
-  // getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-  // getItem('Files', '9', <FileOutlined />),
-];
+const { Header, Content, Footer } = Layout;
+
+const items =[
+  {
+                key: 1,
+                label: <Link to='/'>Home</Link>,
+                // icon:<FontAwesomeIcon icon={faHouseChimney} />
+              },
+              {
+                key: 2,
+                label: <Link to='/projects'>Projects</Link>,
+                // icon:<FontAwesomeIcon icon={faDiagramProject}></FontAwesomeIcon>
+              },
+            ]
 const App = () => {
-  const [collapsed, setCollapsed] = useState(false);
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
+  
+
   return (
-
-    <Layout
-      style={{
-        minHeight: '100vh',
-      }}
-    >
-      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-        <div
-          style={{
-            height: 32,
-            margin: 16,
-            background: 'rgba(255, 255, 255, 0.2)',
-          }}
-        />
-        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
-      </Sider>
-      <Layout className="site-layout">
-        <Header
-          style={{
-            padding: 0,
-            background: colorBgContainer,
-          }}
-        />
-        <Content
-          style={{
-            margin: '0 16px',
-          }}>
-
-          <Routes >
+    <Layout className="layout">
+      <Header>
+      <div style={{float: 'left'}}>
+        <h1 style={{color: 'white', fontSize: '50px',fontFamily: 'Kanit', marginTop: '-6px'}}>gencho.pro</h1>
+        {/* <img
+        width={96}
+        height={54}
+        src='/logo192.png' 
+        alt='logo'/> */}
+      </div> 
+      
+        <div className="logo" />
+        <Menu
+          style={{fontSize: '35px', fontFamily: 'Verdana', color: 'orange', paddingLeft: '200px'}}
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['1']}
+          items={items}
+          />
+      </Header>
+      <Content
+        style={{
+          padding: '0 50px',
+        }}
+      >
+        <Routes >
           {/* Menu */}
           <Route path='/' element={<Home />}/>
         </Routes>
-        </Content>
-        <Footer
-          style={{
-            textAlign: 'center',
-          }}
-        >
-          Ant Design ©2018 Created by Ant UED
-        </Footer>
-      </Layout>
+
+  
+      </Content>
+      <Footer
+        style={{
+          textAlign: 'center',
+        }}
+      >
+        Ant Design ©2018 Created by Ant UED
+      </Footer>
     </Layout>
   );
 };
 export default App;
+
+
+// import { Link, Route, Routes } from 'react-router-dom';
+// import React from 'react';
+// import { Breadcrumb, Layout, Menu, theme } from 'antd';
+// const { Header, Content, Footer } = Layout;
+
+// const items =[
+//   {
+//                 key: 1,
+//                 label: <Link to='/'>Home</Link>,
+//                 // icon:<FontAwesomeIcon icon={faHouseChimney} />
+//               },
+//               {
+//                 key: 2,
+//                 label: <Link to='/projects'>Projects</Link>,
+//                 // icon:<FontAwesomeIcon icon={faDiagramProject}></FontAwesomeIcon>
+//               },
+//             ]
+// const App = () => {
+//   const {
+//     token: { colorBgContainer },
+//   } = theme.useToken();
+//   return (
+//     <Layout className="layout">
+//       <Header>
+//       <div style={{}}>
+//         <img src='' 
+
+//         alt='logo'/>
+//       </div>
+// {/*         
+//         <div className="logo" /> */}
+//         <Menu
+//           theme="dark"
+//           mode="horizontal"
+//           defaultSelectedKeys={['1']}
+//           items={items}
+//           />
+//       </Header>
+//       <Content
+//         style={{
+//           padding: '0 50px',
+//         }}
+//       >
+//         <Breadcrumb
+//           style={{
+//             margin: '16px 0',
+//           }}
+//         >
+//           <Breadcrumb.Item>Home</Breadcrumb.Item>
+//           <Breadcrumb.Item>List</Breadcrumb.Item>
+//           <Breadcrumb.Item>App</Breadcrumb.Item>
+//         </Breadcrumb>
+//         <div
+//           className="site-layout-content"
+//           style={{
+//             background: colorBgContainer,
+//           }}
+//         >
+//         <Routes >
+//           {/* Menu */}
+//           {/* <Route path='/' element={<Home />}/> */}
+//         </Routes>
+
+//         </div>
+//       </Content>
+//       <Footer
+//         style={{
+//           textAlign: 'center',
+//         }}
+//       >
+//         Ant Design ©2018 Created by Ant UED
+//       </Footer>
+//     </Layout>
+//   );
+// };
+// export default App;
+
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,24 @@
-
+import { Image, } from 'antd'
+import { Link } from 'react-router-dom'
 const Home = () => {
     return (
-        <></>
+        <>
+        <section >
+            <div >
+                <h1 className='title'>Hello and Welcome to my website!</h1>
+                <p>My name is Evgeni Kostadinov and I am web developer.
+                    I have several <Link to='/projects'>projects</Link> completed using React JS as Frontend and Django REST as a backend service. 
+                    They are hosted at AWS in Docker Containers serverd with NginX. You can have a look at the code in my <a href='https://github.com/kostadinov1'>GitHub Profile</a>.
+                     I am looking forward to get a lot more experienced and I am exited about the technology that the future will bring us. </p>
+                {/* <Image 
+                src='/images/backgrounds/background-1.jpg' 
+                preview={false}
+                width={1600}
+                height={800}
+                /> */}
+            </div>
+        </section>
+        </>
     )
 }
 

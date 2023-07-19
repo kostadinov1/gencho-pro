e05f9a8f8c38fec14828b5553acd0257d7f46fae
kostadinov1 <evgenikostadinov1987@gmail.com>
2023-07-19 20:38:30 +0300

    added animation to hero subtitle

--- src/components/Home/Hero/Hero.js
+++ src/components/Home/Hero/Hero.js
@@ -12,7 +12,21 @@ function Hero() {
                 <h3>My name is</h3>
             </div>
             <h1>Evgeni Kostadinov</h1>
-            <h2>Web Developer</h2>
+            <ul className={`${styles.web_dev_ul}`}>
+              <li className={`${styles.web_dev_li} ${styles.li_1}`}>W</li>
+              <li className={`${styles.web_dev_li} ${styles.li_2}`}>E</li>
+              <li className={`${styles.web_dev_li} ${styles.li_3}`}>B</li>
+              <li className={`${styles.web_dev_li} ${styles.li_4}`}></li>
+              <li className={`${styles.web_dev_li} ${styles.li_5}`}>D</li>
+              <li className={`${styles.web_dev_li} ${styles.li_6}`}>E</li>
+              <li className={`${styles.web_dev_li} ${styles.li_7}`}>V</li>
+              <li className={`${styles.web_dev_li} ${styles.li_8}`}>E</li>
+              <li className={`${styles.web_dev_li} ${styles.li_9}`}>L</li>
+              <li className={`${styles.web_dev_li} ${styles.li_10}`}>O</li>
+              <li className={`${styles.web_dev_li} ${styles.li_11}`}>P</li>
+              <li className={`${styles.web_dev_li} ${styles.li_12}`}>E</li>
+              <li className={`${styles.web_dev_li} ${styles.li_13}`}>R</li>
+            </ul>
             <p>I create Web Application with React JS and Django REST.</p>
             <Link to={'/projects'}>Projects</Link>
         </div>

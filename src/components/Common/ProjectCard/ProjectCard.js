293df58d95019783f1550b748696d890c9827131
kostadinov1 <evgenikostadinov1987@gmail.com>
2023-07-21 23:13:45 +0300

    modified Project Card Styles

--- src/components/Common/ProjectCard/ProjectCard.js
+++ src/components/Common/ProjectCard/ProjectCard.js
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import styles from './ProjectCard.module.css'
+import { GithubFilled } from '@ant-design/icons';
 
 export const ProjectCard = ({project}) => {
   console.log(project);
@@ -23,12 +24,17 @@ export const ProjectCard = ({project}) => {
     </div>
 
     <div className={`${styles.card_back}`}>
+
       <figure className={`${styles.figure}`}>
         <div className={`${styles.img_bg}`}></div>
         <img className={`${styles.img}`}  src={project.imgUrl} alt=""/>
       </figure>
+      <div className={`${styles.button}`}>
 
-      <button className={`${styles.button}`}><a href={project.link}>Check It Out</a></button>
+      <button className={`${styles.button}`}><a href={project.link}>Web Link</a></button>
+      <button className={`${styles.button}`}><a href={project.link}><GithubFilled /> Frontend</a></button>
+      <button className={`${styles.button}`}><a href={project.link}><GithubFilled /> Backend</a></button>
+      </div>
 
       <div className={`${styles.design_container}`}>
         <span className={`${styles.design} ${styles.design_1}`} ></span>
